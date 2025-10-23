1d86e0c977ee4d6c3f88add8b4f7046a6cc7191d
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-10-23 15:30:31 +0200

    create_route module, no idempotence

---
+++ ansible_collections/bofzilla/purelymail/plugins/modules/create_route.py
@@ -0,0 +1,100 @@
+from ansible.module_utils.basic import AnsibleModule
+
+from ansible_collections.bofzilla.purelymail.plugins.module_utils.clients.base_client import (
+	PurelymailAPI,
+)
+from ansible_collections.bofzilla.purelymail.plugins.module_utils.clients.routing_client import (
+	RoutingClient,
+)
+from ansible_collections.bofzilla.purelymail.plugins.module_utils.clients.types.requests import (
+	CreateRoutingRequest,
+)
+
+DOCUMENTATION = r"""
+---
+module: create_route
+short_description: Create a new routing rule
+description:
+  - This module connects to Purelymail API and creates a new routing rule
+  - WebUI match cases:
+      - [Any address] → C(match_user="", prefix=True, catchall=False)
+      - [Any address except valid user address (catchall)] → C(match_user="", prefix=True, catchall=True)
+      - [Any address starting with] → C(match_user="<anything you want>", prefix=True, catchall=False)
+      - [The exact address] → C(match_user="<anything you want>", prefix=False, catchall=False)
+options:
+  api_token:
+    description: Purelymail API token
+    required: true
+    type: str
+
+  domain_name:
+    description: Domain the rule applies to
+    type: str
+    required: true
+  match_user:
+    description: Local part of the user address
+    type: str
+    required: true
+  target_addresses:
+    description: List of target email addresses
+    type: list
+    elements: str
+    required: true
+  prefix:
+    description: Whether match_user is treated as a prefix
+    type: bool
+    required: true
+  catchall:
+    description: Whether this is a catchall rule
+    type: bool
+	default: false
+    required: false
+
+author:
+  - vic1707
+"""
+
+EXAMPLES = r"""
+- name: Create routing rule
+  bofzilla.purelymail.create_route:
+    api_token: "{{ lookup('env','PURELYMAIL_API_TOKEN') }}"
+
+	domain_name: example.com
+	match_user: admin
+	target_addresses: ["example@example.com"]
+	prefix: false
+	catchall: false
+"""
+
+RETURN = r""""""
+
+
+def main():
+	module = AnsibleModule(
+		argument_spec=dict(
+			api_token=dict(type="str", required=True, no_log=True),
+			domain_name=dict(type="str", required=True),
+			match_user=dict(type="str", required=True),
+			target_addresses=dict(type="list", elements="str", required=True),
+			prefix=dict(type="bool", required=True),
+			catchall=dict(type="bool", required=False, default=False),
+		),
+	)
+
+	api = PurelymailAPI(module, module.params["api_token"])
+	client = RoutingClient(api)
+
+	try:
+		route_spec = module.params
+		del route_spec["api_token"]
+		route = CreateRoutingRequest(**route_spec)
+
+		# TODO: check mode, idempotence
+		_ = client.create_route(route)
+		module.exit_json(changed=True)
+	except Exception as e:
+		module.fail_json(msg=str(e))
+
+
+if __name__ == "__main__":
+	main()
