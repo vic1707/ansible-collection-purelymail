782ced8aed4f2fabd1fcfa95efca917c7d7dccf2
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-11-29 16:20:45 +0100

    wip

---
+++ ansible_collections/bofzilla/purelymail/tests/unit/plugins/lookup/test_purelymail_ownership_code.py
@@ -0,0 +1,27 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from ansible_collections.bofzilla.purelymail.plugins.lookup import purelymail_ownership_code
+from ansible_collections.bofzilla.purelymail.plugins.module_utils.clients.types.responses import GetOwnershipCodeResponse
+
+
+@pytest.fixture(scope="module")
+def lookup_module(monkeypatch: pytest.MonkeyPatch) -> purelymail_ownership_code.LookupModule:
+	domain_client = MagicMock()
+	domain_client.get_ownership_code.return_value = GetOwnershipCodeResponse(code="purelymail_ownership_proof=dQw4w9WgXcQ")
+	monkeypatch.setattr(purelymail_ownership_code, "DomainClient", lambda *_, **__: domain_client)
+
+	return purelymail_ownership_code.LookupModule()
+
+
+def test_lookup_success(lookup_module):
+	ret = lookup_module.run([], variables={}, api_token="dummy")
+	assert isinstance(ret, list)
+	assert len(ret) == 1
+	assert ret[0].code == "purelymail_ownership_proof=dQw4w9WgXcQ"
+	assert ret[0].value == "dQw4w9WgXcQ"
+
+
+# def test_lookup_missing_token(lookup_module):
+# 	ret = lookup_module.run([], variables={}, api_token="dummy")
