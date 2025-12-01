2580e1b5b1a5c219f0e25ff4cc2331d7a1795cd9
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-12-01 11:34:22 +0100

    wip

---
+++ ansible_collections/bofzilla/purelymail/plugins/module_utils/clients/user_client.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+from ansible_collections.bofzilla.purelymail.plugins.module_utils.clients.base_client import PurelymailAPI
+from ansible_collections.bofzilla.purelymail.plugins.module_utils.clients.types.requests import DeleteUserRequest
+from ansible_collections.bofzilla.purelymail.plugins.module_utils.clients.types.responses import EmptyResponse
+
+
+@dataclass()
+class UserClient:
+	api: PurelymailAPI
+
+	# def create_user(self, req: CreateUserRequest) -> EmptyResponse:
+	# 	return self.api.post("/api/v0/createUser", req, EmptyResponse)
+
+	def delete_user(self, req: DeleteUserRequest) -> EmptyResponse:
+		return self.api.post("/api/v0/deleteUser", req, EmptyResponse)
+
+	# def list_users(self) -> ListUserResponse:
+	# 	return self.api.post("/api/v0/listUser", {}, ListUserResponse)
+
+	# def modify_user(self, req: ModifyUserRequest) -> EmptyResponse:
+	# 	return self.api.post("/api/v0/modifyUser", req, EmptyResponse)
+
+	# def get_user(self, req: GetUserRequest) -> GetUserResponse:
+	# 	return self.api.post("/api/v0/getUser", req, GetUserResponse)
+
+	# def upsert_password_reset(self, req: UpsertPasswordResetRequest) -> EmptyResponse:
+	# 	return self.api.post("/api/v0/upsertPasswordReset", req, EmptyResponse)
+
+	# def delete_password_reset(self, req: DeletePasswordResetRequest) -> EmptyResponse:
+	# 	return self.api.post("/api/v0/deletePasswordReset", req, EmptyResponse)
+
+	# def list_password_reset(self, req: ListPasswordResetRequest) -> ListPasswordResetResponse:
+	# 	return self.api.post("/api/v0/listPasswordReset", req, ListPasswordResetResponse)
+
+	# def create_app_password(self, req: CreateAppPasswordRequest) -> CreateAppPasswordResponse:
+	# 	return self.api.post("/api/v0/createAppPassword", req, CreateAppPasswordResponse)
+
+	# def delete_app_password(self, req: DeleteAppPasswordRequest) -> EmptyResponse:
+	# 	return self.api.post("/api/v0/deleteAppPassword", req, EmptyResponse)
